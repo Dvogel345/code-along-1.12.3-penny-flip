f9d6bc14aa60a7ff48ea2d11e2ea2fd0c55befa1
Derek V <DVogel345@gmail.com>
2021-08-19 17:43:45 -0400

    Scoreboard not updating corrrently

--- app.js
+++ app.js
@@ -1,55 +1,59 @@
-let coinFlipButton = document.querySelector('#coinFlip')
-let headsCounterElem = document.querySelector('#heads')
-let tailsCounterElem = document.querySelector('#tails')
-let headsPercentElem = document.querySelector('#heads-percents') 
-let tailsPercentElem = document.querySelector('#tails-percents')
-let coinImage = document.querySelector("#coinImg");
-let headsCount = 0
-let tailsCount = 0
-let totalFlips = 0
-let headsPercent = '0%'
-let tailsPercent = '0%'
+// All flips array  tracks a count for each position
+let allFlips = [0,0]
 
-let headsImage = "./assets/images/penny-heads.jpg"
-let tailsImage = "./assets/images/penny-tails.jpg"
-let headsOrTails = true;
+// Define a function that updates scoreboard so that both buttons can reuse it
+let updateScoreBoard = function () {
+    // calculate total num of flips
+    let total = allFlips[0] + allFlips[1]
 
-coinFlipButton.addEventListener('click', function(event){
-    //show tailside
-    //change to other image
-    if (coinImage.src == headsImage){
-        coinImage.src = headsImage
+    // Create a variable to track percentages
+    let allPercents = [0,0]
+
+    // Before trying to divide, make sure the total is not zero
+    if (total > 0) {
+        //Calculate percentages and update scoreboard for each flip
+        for (let i = 0; i < allFlips.length; i++) {
+            // Get percentage for the roll
+            allPercents[i] = Math.round((allFlips[i] / total) * 100)
+        }
     }
-    else{
-        coinImage.src = tailsImage;
-    }
-    
 
-    //TODO: calculate this
-    let even = true;
-    if (even){
-        headsCount += 1;
+    // Update the values in the scoreboard for each value
+    for (let i = 0; i < allFlips.length; i++) {
+        document.getElementById(`td${i+1}`).textContent = allFlips[i]
+        document.getElementById(`td${i+1}-percent`).textContent = allPercents[i] + '%'
     }
-    else {
-        tailsCount += 1;
-    }
-    totalFlips = headsCount + tailsCount;
+}
 
-    headsPercent = totalFlips / headsCount * 100 
-    tailsPercent = totalFlips / tailsCount * 100
+// Listen for DOMContentLoaded to ensure all HTML and resources
+//  have been loaded before attempting to run code
+document.addEventListener('DOMContentLoaded', function(){
+    // add event listeners and handlers for both buttons
+    document.getElementById('coinFlip').addEventListener('click', function(){
+        // determine coin flip using Math.random()
+        let flippedValue = Math.ceil(Math.random()*2)
 
-    // following line 8 run RanNumGen
-    // if (ranNumGen = even)
-    // adds 1 to scoreBoard and displays img of heads
-    // else (ranNUmGen = odd)
-    // adds 1 to scoreBoard and displays img of tails
+        // Display the result of the flip
+        if (flippedValue == 1) {document.getElementById('message').textContent = `You flipped Tails!`}
+        else {document.getElementById('message').textContent = `You flipped Heads!`}
+
+        // Add one to the count of number of flips for the number
+        // remember arraysare indexed from zero, so its one less that the flipped/ rolled value
+        allFlips[flippedValue - 1] += 1
+
+        // Show different images for each coin
+        if (flippedValue == 1) {document.getElementById('coinImg').src = `assets/images/penny-tails.jpg`}
+        else {document.getElementById('coinImg').src = `assets/images/penny-heads.jpg`}
+        // Update scoreboard
+        updateScoreBoard()
+    })
+    document.getElementById('clear').addEventListener('click', function(){
+        // Resets the flip values to zero
+        allFlips = [0,0]
+    })
+
+    document.getElementById('message').textContent = 'Let\'s Get Flipping!'
+
+    // Update scoreboard 
+    updateScoreBoard()
 })
-
-// formula for heads percentage 
-//1 flip w/ 1 heads and 0 tails = 100%
-// 10 flips w/ 5 heads w/ 5 tails = 50%
-// 11 flips w/ 5 heads w/ 6 tails = 50%
-// heads/totalFlips * 100?
-
-let RandomNumGen = Math.floor(Math.random()*10)
-
